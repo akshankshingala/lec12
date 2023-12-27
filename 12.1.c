53e39f80c6d6e95699be8829bc33934412800681
R7\sat <singalaakshank98@gmail.com>
2023-12-27 18:19:45 +0530

    12

---
+++ 12.1.c
@@ -0,0 +1,110 @@
+/*
+Q.1 Write a Program to create Student Record System for 3 students using structure. Consider the below-mentioned attributes in the Student structure:
+- stu_id
+- stu_name
+- stu_age
+- stu_course
+- stu_city
+- stu_standard
+- stu_school
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+struct Student {
+int id;
+char name[20];
+int age;
+char course[20];
+char city[20];
+int standard;
+char school[20];
+};
+void main()
+{
+
+struct Student s;
+struct Student s1;
+struct Student s2;
+ 
+printf("Enter id: ");
+scanf("%d",&s.id);
+printf("Enter name : ");
+scanf("%s",&s.name);
+printf("Enter age ");
+scanf("%d",&s.age);
+printf("Enter course: ");
+scanf("%s",&s.course);
+printf("Enter city: ");
+scanf("%s",&s.city);
+printf("Enter standard: ");
+scanf("%d",&s.standard);
+printf("Enter school: ");
+scanf("%s",&s.school);
+
+printf("\n\n");
+printf("Enter id: ");
+scanf("%d",&s1.id);
+printf("Enter name : ");
+scanf("%s",&s1.name);
+printf("Enter age ");
+scanf("%d",&s1.age);
+printf("Enter course: ");
+scanf("%s",&s1.course);
+printf("Enter city: ");
+scanf("%s",&s1.city);
+printf("Enter standard: ");
+scanf("%d",&s1.standard);
+printf("Enter school: ");
+scanf("%s",&s1.school);
+
+printf("\n\n");
+printf("Enter id: ");
+scanf("%d",&s2.id);
+printf("Enter name : ");
+scanf("%s",&s2.name);
+printf("Enter age ");
+scanf("%d",&s2.age);
+printf("Enter course: ");
+scanf("%s",&s2.course);
+printf("Enter city: ");
+scanf("%s",&s2.city);
+printf("Enter standard: ");
+scanf("%d",&s2.standard);
+printf("Enter school: ");
+scanf("%s",&s2.school);
+
+printf("\n");
+
+printf("id is  : %d\n",s.id);
+printf("Name is : %s\n",s.name);
+printf("age is  : %d\n",s.age);
+printf("course is   : %s\n",s.course);
+printf("city name is  : %s\n",s.city);
+printf("standard is  : %d\n",s.standard);
+printf("school is  : %s\n",s.school);
+
+printf("\n");
+
+printf("id is  : %d\n",s1.id);
+printf("Name is : %s\n",s1.name);
+printf("age is  : %d\n",s1.age);
+printf("course is   : %s\n",s1.course);
+printf("city name is  : %s\n",s1.city);
+printf("standard is  : %d\n",s1.standard);
+printf("school is  : %s\n",s1.school);
+
+printf("\n");
+
+printf("id is  : %d\n",s2.id);
+printf("Name is : %s\n",s2.name);
+printf("age is  : %d\n",s2.age);
+printf("course is   : %s\n",s2.course);
+printf("city name is  : %s\n",s2.city);
+printf("standard is  : %d\n",s2.standard);
+printf("school is  : %s\n",s2.school);
+
+} 
+
+
